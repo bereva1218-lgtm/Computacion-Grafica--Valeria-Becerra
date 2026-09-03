68aa4eb86ba166fbdff68f1a7401fbddcfc4d7f0
bereva1218-lgtm <you@example.com>
2026-09-03 09:21:29 -0500

    Boton Malla activa

--- src/js/mainClass3.js
+++ src/js/mainClass3.js
@@ -1,23 +1,29 @@
 
 import * as THREE from 'three';
+// los Orbit control son los que necesito para mover el escenario 
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-const scene = new THREE.Scene();
+//TRINIDAD BASICA 
+const scene = new THREE.Scene(); // contenedor donde va estar la escena 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-const renderer = new THREE.WebGLRenderer();
+//es una camara que simula una real, con 75 de campo de vision en grados, aspecto de ancho (pantalla) 
+//0.1 y 1000 son los limites de profundidad, lo que este antes de 0.1 o despues de 100 no se renderiza
+const renderer = new THREE.WebGLRenderer(); // Esto es lo que renderiza todo
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.domElement.style.display = 'block';
-renderer.domElement.style.width = '100%';
+
+//esto lo que hace es crear una variable constante de fondo 
 const backgroundColor = new THREE.Color(0xF2ADFF);
 renderer.setClearColor(backgroundColor);
-renderer.setAnimationLoop(animate);
-document.body.appendChild(renderer.domElement);
 
-const ambientLight = new THREE.AmbientLight( 0xffffff, 0.4 );
+renderer.setAnimationLoop(animate); // le dice al render que llame a la funcion animate en cada frame 
+document.body.appendChild(renderer.domElement); // esto es el CANVAS para que se vea en el html, es como un contenedor donde se va a ver la escena
+
+//LIGHTS
+const ambientLight = new THREE.AmbientLight( 0xffffff, 0.4 ); // sirve para que se ilumine todo por igual, hasta las partes con sombra
 scene.add( ambientLight );
 
-const light = new THREE.DirectionalLight( 0xffffff, 1.2 );
+const light = new THREE.DirectionalLight( 0xffffff, 1.2 ); // simula una fuente cercana de luz 
+// la posicin define desde donde viene esa luz, apunta hacia el origen por defecto
 light.position.set( 5, 10, 7 );
 scene.add( light );
 
@@ -60,33 +66,37 @@ const shapesData = [
 ];
 
 const meshes = [];
-let isWireframe = false;
+let isWireframe = false; // esto es para que las mallas esten por defecto sin activar
 
 shapesData.forEach( ( shapeData ) => {
     const material = new THREE.MeshStandardMaterial( { color: shapeData.color, wireframe: isWireframe, roughness: 0.3, metalness: 0.2} );
+    // MeshStandartMaterial es el que mejor reacciona a la luz
+    // a cada elemento del shapedata, les pone el color que se le puso
     const mesh = new THREE.Mesh( shapeData.geometry, material );
+    //EL MESH ES EL OBJETO QUE SE VA A RENDERIZAR COMO TAL, LA GEOMETRÍA + EL MATERIAL
     mesh.position.x = shapeData.posX;
-    scene.add( mesh );
-    meshes.push( mesh );
+    scene.add( mesh ); // a la escena le agrego el mesh que acabo de crear
+    meshes.push( mesh ); // los agrego al arreglo 
 } );
 
-const controls = new OrbitControls(camera, renderer.domElement);
+//ESTO ES LO DE ORBITS
+const controls = new OrbitControls(camera, renderer.domElement); // vincula la camara y el mouse del render para mver
 camera.position.set(0, -1.5, 16);
 controls.update();
 
 // grilla para guiarme 
 const size = 10;
 const divisions = 10;
-const gridHelper = new THREE.GridHelper(size, divisions);
+const gridHelper = new THREE.GridHelper(size, divisions); // dibuja una grilla en el plano xz
 scene.add(gridHelper);
 
 // para ver las coordenadas x,y,z
-const axesHelper = new THREE.AxesHelper(5);
+const axesHelper = new THREE.AxesHelper(5); // con longitud 5
 scene.add(axesHelper);
 
 
 function animate(time) {
-    renderer.render(scene, camera);
+    renderer.render(scene, camera); // linea que realmente dibuja el frame actual
     controls.update();
 
     meshes.forEach( (mesh ) => {
@@ -94,4 +104,21 @@ function animate(time) {
         mesh.rotation.x = time * speed;
         mesh.rotation.y = time * speed;
     });
+// BOTON PARA ACTIVAR Y DESACTIVAR EL WIREFRAM
 }
+
+const botom = document.getElementById('wireframe-button'); //hay q crear un boton en el html con el id wireframe-button
+function MallaActiva(){
+    isWireframe=!isWireframe;
+    meshes.forEach( (mesh) => {
+        mesh.material.wireframe = isWireframe;
+    })
+    if (isWireframe){
+        botom.innerHTML = "Desactivar Malla";
+    }
+    else{
+        botom.innerHTML = "Activar Malla";
+    }
+}
+botom.addEventListener('click', MallaActiva);
+
